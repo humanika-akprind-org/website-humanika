59170f828c539db0ec93a03b74056cad363d0cf6
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-08-18 13:54:43 +0700

    Fix ESLint configuration errors

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,29 @@ const nextConfig = {
         hostname: "drive.google.com",
         pathname: "/uc/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
+  },
+
+  transpilePackages: ["jsonwebtoken", "bcryptjs", "mongodb"],
+
+  webpack: (config, { isServer }) => {
+    // Gunakan import dinamis untuk polyfill
+    if (!isServer) {
+      config.resolve.fallback = {
+        crypto: false,
+        stream: false,
+        querystring: false,
+      };
+    }
+    return config;
+  },
+
+  experimental: {
+    esmExternals: "loose",
   },
 };
 
