7bfff678241384beaafecc1be55d65d3ceada9f3
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-09-02 22:01:11 +0700

    feat(utils/date): utils date period

---
+++ app/utils/date.ts
@@ -0,0 +1,12 @@
+// utils/date.ts
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return (
+    date.toLocaleDateString() +
+    " " +
+    date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+  );
+};
