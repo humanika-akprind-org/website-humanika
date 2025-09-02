897bcc50829988dd7f5dc0ebf8fba7c3efe5f641
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-09-02 22:10:17 +0700

    feat(components): add components NavDropdownItem view

---
+++ components/admin/layout/NavDropdownItem.tsx
@@ -0,0 +1,30 @@
+// components/admin/NavDropdownItem.tsx
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
+
+interface NavDropdownItemProps {
+  href: string;
+  children: ReactNode;
+}
+
+export default function NavDropdownItem({
+  href,
+  children,
+}: NavDropdownItemProps) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center space-x-3 p-2 rounded-lg text-gray-500 hover:bg-blue-50 hover:text-blue-600 transition-colors text-xs ml-3 ${
+        isActive ? "bg-blue-50 text-blue-600 font-medium" : ""
+      }`}
+    >
+      <span>{children}</span>
+    </Link>
+  );
+}
