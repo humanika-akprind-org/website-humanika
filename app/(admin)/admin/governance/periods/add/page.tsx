419761a1ff94d3f08b75fb65aee3ea3f9bbbbffa
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-09-02 21:58:47 +0700

    feat(page/periods/add): implement periods add management page

---
+++ app/(admin)/admin/governance/periods/add/page.tsx
@@ -0,0 +1,9 @@
+import PeriodForm from "@/components/admin/period/Form";
+
+export default function AddPeriodPage() {
+  return (
+    <div className="p-6">
+      <PeriodForm />
+    </div>
+  );
+}
