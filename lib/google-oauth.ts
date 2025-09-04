a2d1c790ac6c8f6ecd9ad592fe805e303ce66a79
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-09-04 12:51:11 +0700

    fix: bug fixes in management module

--- lib/google-oauth.ts
+++ lib/google-oauth.ts
@@ -6,6 +6,14 @@ export const oauth2Client = new google.auth.OAuth2(
   process.env.REDIRECT_URI
 );
 
+// Set credentials if available
+if (process.env.ACCESS_TOKEN && process.env.REFRESH_TOKEN) {
+  oauth2Client.setCredentials({
+    access_token: process.env.ACCESS_TOKEN,
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+}
+
 export const drive = google.drive({
   version: "v3",
   auth: oauth2Client,
